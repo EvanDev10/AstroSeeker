92069242ae39d0533c0c04c6c94af7b5f853c80b
EvanDev10 <kanekivo@gmail.com>
2022-05-28 15:43:43 -0500

    Update
    Se agrego la funcionalidad con firebase

--- screens/Community.js
+++ screens/Community.js
@@ -1,43 +1,102 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SafeAreaView, TouchableOpacity, Alert, TextInput, Modal, FlatList, StyleSheet, Text, View, Pressable, Image } from 'react-native';
-import { Stack, Button, NativeBaseProvider, Box  } from "native-base";
+import { Stack, Divider, Input, Switch, Button, NativeBaseProvider, Box } from "native-base";
+import fb from '../Firebase/Conexion';
+import { doc, getDocs, startAt, where,orderBy, collection, limit, query } from 'firebase/firestore';
 
-    const Datos = [
-        {
-            Tema: "Nuevas investigaciones" ,
-            Autor: "Javier",
-            Siguiendo: "X",
-        },
-        {
-            Tema: "UPDATE URGENTE" ,
-            Autor: "Alan",
-            Siguiendo: "O",
-        },
-        {
-            Tema: "Aplicacion con delay" ,
-            Autor: "Alejandro",
-            Siguiendo: "X",
-        },
-        {
-            Tema: "UPDATE URGENTE" ,
-            Autor: "Alan",
-            Siguiendo: "O",
-        },
-        {
-            Tema: "Aplicacion con delay" ,
-            Autor: "Alejandro",
-            Siguiendo: "X",
-        },
-      ];
+const Community = ({ navigation }) => {
 
-const Community = ({navigation}) => {
+    const FechaDeHoy = () => {
+
+        const Dia = new Date().getDate();
+        const Mes = new Date().getMonth() + 1;
+        const Ano = new Date().getFullYear();
+
+        const Meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+        return "".concat(Dia, "-", Meses[Mes - 1], "-", Ano);
+
+    }
+
+    useEffect(() => {
+        BuscarInfo();
+        console.log("Hecho")
+        console.log("Temas:\n", Temas)
+    }, [Bus]);
+
+    //Listado de 10 post 
+    const [Temas, setTemas] = useState({
+        Id: "",
+        Autor: "",
+        Tema: "",
+        Descripcion: "",
+        Fecha: ""
+    })
+
+    const [Bus, setBus] = useState("")
+
+    const handleBus = (e) => { setBus(e.target.value); console.log("Bus: ", e.target.value) }
+
+    const handleTema = (e) => {
+        setTemas(e);
+        console.log(Temas)
+    }
+
+    const BuscarInfo = async () => {
+        console.log("Inicia")
+        //Referencia a la BD en su tabla topicos
+        const dbRef = collection(fb.db, "Topicos");
+
+        const Fila = [];
+
+        if (Bus != "") {
+            const Busqueda = query(dbRef, orderBy("Tema"), startAt(Bus));
+
+            const querySnapshot = await getDocs(Busqueda);
+
+            querySnapshot.forEach((doc) => {
+                const Id = doc.id;
+                const { Autor, Tema, Descripcion, Fecha } = doc.data();
+                Fila.push({
+                    Id,
+                    Autor,
+                    Tema,
+                    Descripcion,
+                    Fecha
+                });
+            });
+            handleTema(Fila);
+            console.log("Temas", Temas);
+        }
+        else {
+            const Busqueda = query(dbRef, limit(10));
+            const querySnapshot = await getDocs(Busqueda);
+
+            querySnapshot.forEach((doc) => {
+                const Id = doc.id;
+                const { Autor, Tema, Descripcion, Fecha } = doc.data();
+                Fila.push({
+                    Id,
+                    Autor,
+                    Tema,
+                    Descripcion,
+                    Fecha
+                });
+            });
+            handleTema(Fila);
+            console.log("Fila:\n", Fila);
+        }
+        console.log("Fin");
+        return Fila;
+    }
+
+
     //HowTo Propio
-
     const [showModal, setShowModal] = useState(false)
 
     const HowTo = () => {
-    
+
         return (
             <View>
                 <Modal
@@ -45,96 +104,99 @@ const Community = ({navigation}) => {
                     transparent={false}
                     visible={showModal}
                     onRequestClose={() => {
-                        Alert.alert("Hasta la proxima c:");
+                        alert("Hasta la proxima c:");
                         setShowModal(!showModal);
                     }}
-                    >
-                    <View style = {{alignItems: 'center', padding: 22, margin: 5}}>
-                        <Text style = {{alignItems: 'center', padding: 22, margin: 5}}>Bienvenido</Text>
-                        <Text style = {{alignItems: 'center', padding: 22, margin: 5}}>AQUI SE MUESTRAN LAS DIFERENTES INSTRUCCIONES PARA MANEJAR LA APLICACION")</Text>
-                        <Stack space = {3} direction = 'row'>
-                            <Button style = {{ width: 100}}>{"<-"}</Button>
-                            <Button style = {{ width: 100}}>X</Button>
-                            <Button style = {{ width: 100}}>{"->"}</Button>
-                            </Stack>
+                >
+                    <View style={{ alignItems: 'center', padding: 22, margin: 5 }}>
+                        <Text style={{ alignItems: 'center', padding: 22, margin: 5 }}>Bienvenido</Text>
+                        <Text style={{ alignItems: 'center', padding: 22, margin: 5 }}>AQUI SE MUESTRAN LAS DIFERENTES INSTRUCCIONES PARA MANEJAR LA APLICACION")</Text>
+                        <Stack space={3} direction='row'>
+                            <Button style={{ width: 100 }}>{"<-"}</Button>
+                            <Button style={{ width: 100 }} onPress={() => setShowModal(!showModal)}> X</Button>
+                            <Button style={{ width: 100 }}>{"->"}</Button>
+                        </Stack>
                     </View>
                 </Modal>
             </View>
         );
     }
-    
-    //Componente del foro
-    const Topicos = (Valores) => {
-        return (
-            <View style = {Estilo.Marco}>
-                <Stack direction = "column" space = {2} style = {Estilo.Caratula}>
-                    <Pressable style = {{height: Estilo.Titulo.height, width: Estilo.Titulo.width}} onPress = {()=> { navigation.navigate("ShowPost") }}>
-                        <Text style = {Estilo.Titulo}>Tema</Text>
-                        </Pressable>
-                    <Stack direction = "row" space = {2} style = {Estilo.Caratula2}>
-                        <Stack direction = "row" space = {2} style = {Estilo.Caratula3}>
-                            <Image alt = "Foto" style = {{borderColor: 'black', borderWidth: 2, borderRadius: 20, width: 45, height: 45}}></Image>
-                            <Text style = {Estilo.Texto}>Autor</Text>
-                            </Stack>
-                        <Button style = {Estilo.BotonEstilo} onPress = {() => navigation.navigate("How")}>Seguir</Button>
-                        </Stack>
-                    </Stack>
-                </View>
-        );
-    }
 
-    const BotonFlotante = () =>{
-        return (
-            <SafeAreaView>
-            <View>
-                <TouchableOpacity
-                    activeOpacity={0.7}
-                    onPress={()=> { navigation.navigate("newPost")}}
-                    style={Estilo.TouchableFlotante}>
-                    <Image
-                        source={require("./../assets/Agregar.png")}
-                        style={Estilo.BotonFlotante}/>
-                </TouchableOpacity>
-                </View>
-            </SafeAreaView>
-        );
-    }
-
-    const BotonFlotante2 = () =>{
+    const NewPostButton = () => {
         return (
             <SafeAreaView>
                 <View>
                     <TouchableOpacity
                         activeOpacity={0.7}
-                        onPress={()=> { setShowModal(!showModal) } }
-                        style={Estilo.TouchableFlotante2}>
+                        onPress={() => { navigation.navigate("newPost") }}
+                        style={Estilo.TouchableFlotante}>
                         <Image
-                            source={require("./../assets/Question.png")}
-                            style={Estilo.BotonFlotante2}/>
-                    
+                            source={require("./../assets/Agregar.png")}
+                            style={Estilo.BotonFlotante} />
                     </TouchableOpacity>
-                    </View>
+                </View>
             </SafeAreaView>
         );
     }
-    
+
+    const HowToButton = () => {
+        return (
+            <SafeAreaView>
+                <View>
+                    <TouchableOpacity
+                        activeOpacity={0.7}
+                        onPress={() => { setShowModal(!showModal) }}
+                        style={Estilo.TouchableFlotante2}>
+                        <Image
+                            source={require("./../assets/Question.png")}
+                            style={Estilo.BotonFlotante2} />
+
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        );
+    }
+
+    //Componente del foro
+    const Topicos = ({ item }) => {
+        
+        return (
+            <View style={Estilo.Marco}>
+                <Stack>
+                    <Pressable style={{ height: Estilo.Titulo.height, width: Estilo.Titulo.width }} onPress={() => 
+                        { navigation.navigate("ShowPost", { idPost: item.id, Autor: item.Autor, Descripcion: item.Descripcion, Tema: item.Tema }) }}>
+                        <Text style={Estilo.Titulo}>{item.Tema}</Text>
+                    </Pressable>
+                    <Stack direction="row" space={2} style={Estilo.Caratula2}>
+                        <Stack  style = {{width: 800}}direction="row" space={2} style={Estilo.Caratula3}>
+                            <Image alt="Foto" style={{ borderColor: 'black', borderWidth: 2, borderRadius: 40, width: 45, height: 45 }}></Image>
+                            <Text style={Estilo.Texto}>{item.Autor}</Text>
+                        </Stack>
+                    </Stack>
+                </Stack>
+            </View>
+
+        );
+    }
+
     return (
         <NativeBaseProvider>
-            <Box flex = {10} style = {Estilo.Contenedor}>
-                <FlatList
-                    data={Datos}
-                    renderItem={Topicos}
-                    style = {{height: 100}}/>
-                </Box>
-                <Pressable onPress = {()=> { navigation.navigate("newPost")}}>
-                    
-                    </Pressable>
-                <HowTo/>
-				<BotonFlotante/>
-                <BotonFlotante2/>
-			</NativeBaseProvider>
+            <Box flex={10} style={Estilo.Contenedor}>
+                <Input value={Bus} onChange={handleBus} style={Estilo.InputBus} type="text" w="full" maxW="300px" py="0" InputRightElement=
+                    {<Button size="xs" rounded="none" h="full" onPress={BuscarInfo}>Search
+                    </Button>} placeholder="Tipea tu busqueda..." />
+                <Divider />
+
+                <FlatList data={Temas} renderItem={Topicos} />
+
+                <HowTo />
+            </Box>
+            <NewPostButton />
+            <HowToButton />
+        </NativeBaseProvider>
 
     );
+
 }
 
 //CSS
@@ -143,7 +205,14 @@ const Estilo = StyleSheet.create({
         backgroundColor: '#0df',
         alignItems: 'center',
         justifyContent: 'center',
-        },
+    },
+    InputBus: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        margin: 3,
+        padding: 5,
+        justifyContent: 'center',
+    },
     Texto: {
         backgroundColor: '#a64',
         color: 'white',
@@ -153,7 +222,7 @@ const Estilo = StyleSheet.create({
         margin: 2,
         height: 50,
         width: '70%',
-        },
+    },
     Caratula: {
         width: '99%',
         alignItems: 'center',
@@ -162,7 +231,7 @@ const Estilo = StyleSheet.create({
         borderRadius: 8,
         padding: 2,
         margin: 2
-        },
+    },
     Caratula2: {
         width: '70%',
         alignItems: 'center',
@@ -172,7 +241,7 @@ const Estilo = StyleSheet.create({
         borderRadius: 8,
         padding: 0,
         margin: 15
-        },
+    },
     Caratula3: {
         width: '70%',
         alignItems: 'center',
@@ -182,10 +251,10 @@ const Estilo = StyleSheet.create({
         borderRadius: 8,
         padding: 0,
         margin: 15
-        },
+    },
     Lista: {
         width: '100%'
-        },
+    },
     Marco: {
         backgroundColor: '#fff',
         padding: 8,
@@ -195,7 +264,7 @@ const Estilo = StyleSheet.create({
         borderRadius: 15,
         height: 150,
         width: 300,
-        },
+    },
     Titulo: {
         backgroundColor: '#646',
         padding: 10,
@@ -205,7 +274,7 @@ const Estilo = StyleSheet.create({
         height: 40,
         textAlign: 'center',
         borderRadius: 10
-        },
+    },
     BotonEstilo: {
         backgroundColor: '#000',
         padding: 8,
@@ -216,7 +285,7 @@ const Estilo = StyleSheet.create({
         borderRadius: 15,
         height: 45,
         width: 70,
-        },
+    },
     TouchableFlotante: {
         position: 'absolute',
         width: 65,
@@ -225,30 +294,30 @@ const Estilo = StyleSheet.create({
         justifyContent: 'center',
         right: 15,
         bottom: 15,
-      },
+    },
     BotonFlotante: {
         resizeMode: 'contain',
         width: 65,
         height: 65,
         //backgroundColor:'black'
-      },
-      TouchableFlotante2: {
-          position: 'absolute',
-          width: 65,
-          height: 65,
-          alignItems: 'center',
-          justifyContent: 'center',
-          left: 15,
-          bottom: 15,
-        },
-      BotonFlotante2: {
-          resizeMode: 'contain',
-          width: 65,
-          height: 65,
-          //backgroundColor:'black'
-        }
+    },
+    TouchableFlotante2: {
+        position: 'absolute',
+        width: 65,
+        height: 65,
+        alignItems: 'center',
+        justifyContent: 'center',
+        left: 15,
+        bottom: 15,
+    },
+    BotonFlotante2: {
+        resizeMode: 'contain',
+        width: 65,
+        height: 65,
+        //backgroundColor:'black'
+    }
 
-    });
+});
 
 
 export default Community;
--- screens/NewPost.js
+++ screens/NewPost.js
@@ -1,40 +1,131 @@
-import React from "react";
-import { Alert, TextInput, Modal, FlatList, StyleSheet, Text, View, Pressable, Image } from 'react-native';
-import { Stack, Button, NativeBaseProvider, Box, Circle } from "native-base";
+import { React, useState } from "react";
+import { Alert, Modal, FlatList, StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { Stack, Divider, TextArea, Input, Button, NativeBaseProvider, Box, Circle } from "native-base";
+import { collection, addDoc, doc, setDoc } from 'firebase/firestore';
+import fb from '../Firebase/Conexion'
+import { setStatusBarNetworkActivityIndicatorVisible } from "expo-status-bar";
 
-const NewPost = ({navigation}) => {
+const NewPost = ({ navigation }) => {
+
+
+    const [Usr, setUsr] = useState("Carlos") 
+    //const CargarUsrLogeado = () => {
+        //Aqui se asigna al usuario logeado con el Auth
+        //Lo dejo fijo, para que lo asignes javys
+   //     setUsr("");
+   // }
+
+    const FechaDeHoy = () => {
+
+        const Dia = new Date().getDate();
+        const Mes = new Date().getMonth() + 1;
+        const Ano = new Date().getFullYear();
+
+        const Meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+        return "".concat(Dia, "-", Meses[Mes - 1], "-", Ano);
+
+    }
+
+    const FechaFinal = "".concat(new Date().getDate(), "/", new Date().getMonth() + 1, "/", new Date().getFullYear())
+
+    const [showModal, setShowModal] = useState(false)
+
+    const changeShowModal = () => setShowModal(!showModal)
+
+    const ProcederModal = () => {
+
+        return (
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => {
+                    alert("Hasta luego");
+                    setShowModal(!showModal);
+                }}
+            >
+                <View style={Estilo.ModalProc}>
+                    <Text style={{ alignItems: 'center', padding: 22, margin: 5 }}>Antes de continuar...</Text>
+                    <Text style={{ alignItems: 'center', padding: 22, margin: 5 }}>¿Realmente desea publicar este post?</Text>
+                    <Stack space={2} direction='coulumn'>
+                        <Button style={{ width: 100 }} onPress={CrearPost}>Continuar</Button>
+                        <Button style={{ width: 100 }} onPress={changeShowModal}>Cancelar</Button>
+                    </Stack>
+                </View>
+            </Modal>
+        );
+    }
+
+    const BotonVolver = () => {
+        return (
+            <Button style={Estilo.BotonVolver} onPress={() => { navigation.navigate("Comm") }}>Volver</Button>
+        );
+    }
+
+    const CrearPost = async () => {
+
+        if (Topico.Tema != "" && Topico.Descripcion != "") {
+            const docRef = await addDoc(collection(fb.db, "Topicos"), {
+                Autor: Topico.Autor,
+                Tema: Topico.Tema,
+                Descripcion: Topico.Descripcion,
+                Fecha: FechaFinal
+            });
+
+            if (docRef.id != "")
+                alert("Registro exitoso");
+            else
+                alert("Hubo un error al registrar");
+        }
+        else {
+            alert("No deje los campos vacios")
+        }
+
+    }
+
+    const [Topico, setTopico] = useState({
+        Autor: "Javys", //Reemplazar por logeado
+        Tema: "",
+        Descripcion: "",
+        Fecha: FechaDeHoy
+    })
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        setTopico({ ...Topico, [e.target.placeholder]: e.target.value });
+    }
 
     return (
-        <NativeBaseProvider style = {Estilo.Contenedor}>
-                <Box>
-                    <Stack direction = "column" space = {4} style = {Estilo.Contenedor}> 
-                        <Stack style = {Estilo.BordeExterior} direction = "column" space = {2}>
-                            <Text style = {Estilo.Titulo}>Topico:</Text>
-                            <TextInput style = {Estilo.Inputs} editable maxLength={40} >Topico</TextInput>
+        <NativeBaseProvider style={Estilo.Contenedor}>
+            <ProcederModal />
+            <Box style={Estilo.Contenedor}>
+                <Box style={Estilo.Marco}>
+
+                    <Stack direction="column" space={4}>
+
+                        <Stack style={Estilo.BordeExterior} direction="column" space={2}>
+                            <Stack style={Estilo.BordeExterior} direction="row" space={2}>
+                                <Text style={Estilo.Fecha}><FechaDeHoy /></Text>
+                                <BotonVolver />
                             </Stack>
-                        <Stack style = {Estilo.BordeExterior} direction = "column" space = {2}>
-                            <Text style = {Estilo.Titulo}>Descripcion:</Text>
-                            <TextInput multiline style = {Estilo.Inputs} editable maxLength={300} >Descripcion del Topico</TextInput>
+
+                            <Divider />
+                            <Text style={Estilo.Titulo}>Topico:</Text>
+                            <Input value={Topico.Tema} onChange={handleChange} variant="rounded" mx="3" placeholder="Tema" />
+                            <Stack style={Estilo.BordeExterior} direction="column" space={2}>
+                                <Text style={Estilo.Titulo}>Descripcion:</Text>
+                                <TextArea onChange={handleChange} value={Topico.Descripcion} variant="rounded" placeholder="Descripcion" Width={Estilo.Inputs.width} />
                             </Stack>
-                        <Stack style = {Estilo.BordeExterior} direction = "column" space = {2}>    
-                            <Text style = {Estilo.Titulo}>Tags:</Text>
-                            <TextInput style = {Estilo.Inputs} editable maxLength={40} >Tags</TextInput>
-                            </Stack>
-                        <Button style = {Estilo.BotonAceptar} onPress = {() => {Alert.alert("Guardado","¿Seguro que deseas guardar?", 
-                            [
-                                {
-                                    text: "No",
-                                    onPress: () => console.log("Cancelado")
-                                    },
-                                { 
-                                    text: "Si", 
-                                    onPress: () => console.log("En Proceso") 
-                                    }
-                                ])}}>Publicar</Button>
+                            <Divider />
+                            <Button style={Estilo.BotonAceptar} onPress={changeShowModal}>Publicar</Button>
                         </Stack>
-                    </Box>
-              
-            </NativeBaseProvider>
+
+                    </Stack>
+                </Box>
+            </Box>
+
+        </NativeBaseProvider>
 
     );
 }
@@ -45,21 +136,19 @@ const Estilo = StyleSheet.create({
         backgroundColor: '#0df',
         alignItems: 'center',
         justifyContent: 'center',
+        alignContent: 'center',
         width: '100%',
         height: '100%',
-        },
+    },
     Inputs: {
         backgroundColor: 'black',
-        color: 'white',
-        width: 200,
+        width: "70%",
         height: 50,
         justifyContent: "center",
         borderRadius: 20,
-        padding: 4,
-        margin: 5,
-        borderColor: 'red',
-        borderWidth: 1,
-        },
+        padding: 6,
+        margin: 6,
+    },
     Titulo: {
         padding: 2,
         marginLeft: 6,
@@ -68,12 +157,21 @@ const Estilo = StyleSheet.create({
         width: '100%',
         textAlign: 'center',
         borderRadius: 10
-        },
+    },
+    Fecha: {
+        fontWeight: 'bold',
+        width: '100%',
+        textAlign: 'left',
+    },
     BordeExterior: {
-        borderWidth: 2,
-        borderColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
         borderRadius: 20,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        padding: 10,
+        margin: 1,
+        width: '100%'
     },
     BotonAceptar: {
         backgroundColor: '#000',
@@ -87,11 +185,38 @@ const Estilo = StyleSheet.create({
         fontSize: 15,
         height: 60,
         width: '94%',
-        },
-    Marco: {
-        borderRadius: 80
+        fontWeight: 'bold'
     },
-    });
+    Marco: {
+        borderRadius: 80,
+        padding: 10,
+        width: "80%",
+        height: "50%"
+    },
+    ModalProc: {
+        width: 300,
+        height: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: "center",
+        padding: 22,
+        margin: 5,
+        borderWidth: 1,
+        borderRadius: 50
+    },
+    BotonVolver: {
+        backgroundColor: '#000',
+        color: 'white',
+        borderColor: 'red',
+        borderWidth: 2,
+        borderRadius: 15,
+        fontWeight: 'bold',
+        fontSize: 15,
+        height: 40,
+        width: 200,
+        fontWeight: 'bold'
+    }
+});
 
 
 
